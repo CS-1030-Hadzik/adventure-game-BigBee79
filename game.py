53797c7429fbd27dc78182c2a20cc694223e93ce
Your Name <brianwinn@mail.weber.ed>
2025-01-25 17:10:37 -0700

    ADD: Input for player name

--- game.py
+++ game.py
@@ -8,6 +8,10 @@ to navigate through a mysterious forest.
 '''
 
 
-#Welcome message and introduciton
+# Welcome message and introduciton
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...")
+
+# Ask for the player's name
+player_name = input("What is your name, adventurer? ")
+print("Welcome, " + player_name + " Your journey begins now...")
